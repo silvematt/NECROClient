0603c843e21ef4f6c9de29a6be0d01bf72dac2a4
silvematt <devsilvematt@gmail.com>
2024-06-14 14:05:51 +0200

    Console is now limited in lines and history can be browsed with arrow keys

--- Console.cpp
+++ Console.cpp
@@ -3,17 +3,63 @@
 
 int NECROConsole::Init()
 {
-	inputField.Init(SDL_Rect{ 0,0,309,52}, SDL_Rect{ 25, SCREEN_HEIGHT - 120, 360,35 }, "", engine.GetAssetsManager().GetImage("default_input_field.png"), engine.GetAssetsManager().GetImage("default_active_input_field.png"), 0);
+	inputField.Init(SDL_Rect{ 0,0,309,52}, SDL_Rect{ 25, SCREEN_HEIGHT - 120, 380,35 }, "", engine.GetAssetsManager().GetImage("default_input_field.png"), engine.GetAssetsManager().GetImage("default_active_input_field.png"), 0);
 	return 0;
 }
 
+void NECROConsole::Toggle()
+{
+	active = !active;
+	curLineOffset = 0;
+	curHistoryIndex = -1;
+	inputField.SetFocused(active);
+
+	// Send command if(!active)
+	if (!active)
+	{
+		if (inputField.str.size() > 0)
+			history.push_back(inputField.str);
+	}
+}
+
+
 void NECROConsole::Update()
 {
-	if (engine.GetInput().GetKeyDown(SDL_SCANCODE_RETURN))
+	NECROInput& i = engine.GetInput();
+
+	if (i.GetKeyDown(SDL_SCANCODE_RETURN) || i.GetKeyDown(SDL_SCANCODE_KP_ENTER))
 		Toggle();
 
 	if (active)
 	{
+		if (i.GetKeyDown(SDL_SCANCODE_PAGEUP))
+			curLineOffset += CONSOLE_MAX_LINES_PER_HISTORY;
+		else if (i.GetKeyDown(SDL_SCANCODE_PAGEDOWN))
+			curLineOffset -= CONSOLE_MAX_LINES_PER_HISTORY;
+
+		curLineOffset = SDL_clamp(curLineOffset, 0, history.size());
+
+		// Browse history with arrows
+		if (i.GetKeyDown(SDL_SCANCODE_UP))
+		{
+			curHistoryIndex += 1;
+			curHistoryIndex = SDL_clamp(curHistoryIndex, 0, history.size() - 1);
+
+			int index = history.size() - 1 - curHistoryIndex;
+			if (index >= 0 && index <= history.size() - 1)
+				inputField.str = history[index];
+
+		}
+		else if (i.GetKeyDown(SDL_SCANCODE_DOWN))
+		{
+			curHistoryIndex -= 1;
+			curHistoryIndex = SDL_clamp(curHistoryIndex, 0, history.size() - 1);
+
+			int index = history.size() - 1 - curHistoryIndex;
+			if (index >= 0 && index <= history.size() - 1)
+				inputField.str = history[index];
+		}
+
 		Draw();
 	}
 }
@@ -26,22 +72,15 @@ void NECROConsole::Draw()
 	int count = 1;
 	for (int i = history.size()-1; i >= 0; i--)
 	{
-		engine.GetRenderer().DrawTextDirectly(engine.GetAssetsManager().GetFont("defaultFont"), history[i].c_str(), 25, (SCREEN_HEIGHT - 120) - 40 * count, colorWhite);
+		int actualOffset = 0;
+
+		if(i-curLineOffset < history.size())
+			engine.GetRenderer().DrawTextDirectly(engine.GetAssetsManager().GetFont("defaultFont"), history[i - curLineOffset].c_str(), 25, (SCREEN_HEIGHT - 120) - 40 * count, colorWhite);
+
+		// Limit console
+		if (count >= CONSOLE_MAX_LINES_PER_HISTORY)
+			break;
 
 		count++;
-	}
-}
-
-void NECROConsole::Toggle()
-{
-	active = !active;
-
-	inputField.SetFocused(active);
-
-	// Send command if(!active)
-	if (!active)
-	{
-		if(inputField.str.size() > 0)
-			history.push_back(inputField.str);
 	}
 }
--- Console.h
+++ Console.h
@@ -6,6 +6,7 @@
 
 #include "InputField.h"
 
+const int CONSOLE_MAX_LINES_PER_HISTORY = 5;
 const int CONSOLE_MAX_STR_SIZE = 128;
 const int CONSOLE_MAX_STR_STORED = 2048;  // To limit history
 const float	CONSOLE_FADE_OUT_TIME = 5.0f; // after 5 seconds, the messages will fade out until the console is toggled again
@@ -19,8 +20,10 @@ private:
 	bool active;
 	std::vector<std::string> history; // contains all the commands, messages and info the console logs.
 
-	
+	int curLineOffset = 0; // offset in showing history
 	InputField inputField;
+
+	int curHistoryIndex = -1; // to press arrow-up/down and browse history
 
 public:
 	int			Init();
